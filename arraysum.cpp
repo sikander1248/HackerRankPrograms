78f675d785b6896c4e35f56cb2edc87c62692578
MOHAMMED SIKANDER <sikander86@yahoo.com>
2016-08-16 12:35:29 +0530

    replaced array with vector

--- arraysum.cpp
+++ arraysum.cpp
@@ -1,25 +1,23 @@
 
 #include <iostream>
-
 using namespace std;
-
-int main( )
-{
-    int numberOfElements;
-
-    cin >> numberOfElements;
-
-    int arr[numberOfElements];
-    
-    for(int i = 0 ; i < numberOfElements ; i++)
-    {
-        cin >> arr[i];    
-    }
+int calculateArraySum(vector <int> v){
     int sum = 0;
-    for(int ele : arr )
+    for(int ele : v )
     {        
         sum = sum + ele;
     } 
-    
+    return sum;
+}
+int main( )
+{
+    int numberOfElements;
+    cin >> numberOfElements;
+    vector <int>  v(numberOfElements);
+    for(int i = 0 ; i < numberOfElements ; i++)
+    {
+        cin >> v[i];    
+    }
+    int sum = calculateArraySum(v);
     cout << sum;
 }
