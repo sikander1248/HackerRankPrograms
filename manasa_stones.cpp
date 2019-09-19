1a17ca3679eb8fd04b8a02dbc4190b96962e7b60
MOHAMMED SIKANDER <sikander1248@gmail.com>
2019-09-19 09:08:29 +0530

    Updated using Iteration

--- manasa_stones.cpp
+++ manasa_stones.cpp
@@ -1,21 +1,20 @@
+
 #include <iostream>
 #include <stdio.h>
 #include <set>
 #include <string>
 using namespace std;
 
-set<int> s;
-void findLastStoneValue(int a,int b, int n,int stoneValue,string path)
+
+set<int> findLastStoneValue(int a,int b, int n)
 {
-    if( n == 1)
+    set<int> s;
+
+    for(int i = 0 ; i < n ; i++)
     {
-        //printf(" %d",stoneValue);
-        //cout << path << endl;
-        s.insert(stoneValue);    
-        return;
-    }
-    findLastStoneValue(a, b, n - 1, stoneValue + a, path +" a"); //Left
-    findLastStoneValue(a, b, n - 1, stoneValue + b, path + " b"); //Left
+        s.insert( (b * i) + (a * (n - i - 1) ));
+    }    
+    return s;
 }
 int main()
 {
@@ -31,12 +30,11 @@ int main()
         //printf("Enter the value of a and b : ");
         scanf(" %d %d",&a, &b);
 
-        findLastStoneValue(a , b , n , 0 , "");
+        set<int> s = findLastStoneValue(a , b , n );
 
         for(int ele : s)
             cout << ele << " ";
         cout <<endl;
-        s.clear();
     }
 
     
